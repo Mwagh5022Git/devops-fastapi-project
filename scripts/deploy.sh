038bd401d9539cbec39dc913e5c39350b6f3f84c
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-10 13:08:21 +0530

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Starting Terraform"
+
+cd terraform
+
+terraform init
+terraform apply -auto-approve
+
+IP=$(terraform output -raw instance_ip)
+
+echo "Instance IP: $IP"
+
+cd ..
+
+echo "Updating Ansible inventory"
+
+echo "[server]" > ansible/inventory.ini
+echo "$IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/devops-key.pem" >> ansible/inventory.ini
+
+echo "Running Ansible"
+
+ansible-playbook -i ansible/inventory.ini ansible/install_app.yml
