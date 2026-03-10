6d46f30bd92a93a7d022919614469b50fe1eab5f
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-10 13:10:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
+from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 
+app = FastAPI()
+
+REQUEST_COUNT = Counter("request_count", "Total Requests")
+
+@app.get("/")
+def home():
+    REQUEST_COUNT.inc()
+    return {"message": "DevOps FastAPI Project Running"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
