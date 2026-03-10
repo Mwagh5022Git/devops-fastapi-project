8f14331ede6d7a9986ce618b91c85c8c68a33f41
Manish Wagh <167597076+Mwagh5022Git@users.noreply.github.com>
2026-03-10 13:06:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "devops_server" {
+
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  key_name = "devops-key"
+
+  tags = {
+    Name = "DevOps-FastAPI-Server"
+  }
+}
